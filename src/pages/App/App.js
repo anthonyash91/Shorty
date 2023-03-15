7c27591f3fe030f23451d9692a5552de53791ae2
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2023-03-15 01:01:28 -0400

    fixed url issue

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { getUser } from '../../utilities/users-service'
-import UserDashboard from '../UserDashboard/UserDashboard'
-import HomePage from '../HomePage/HomePage'
-import LinkTree from '../LinkTree/LinkTree'
+import { getUser } from '../../utilities/users-service';
+import UserDashboard from '../UserDashboard/UserDashboard';
+import HomePage from '../HomePage/HomePage';
+import LinkTree from '../LinkTree/LinkTree';
 
-function App () {
-  const [user, setUser] = useState(getUser())
-  const [domainName, setDomainName] = useState('shorty-url-ga.herokuapp.com')
-  const [globalLink, setGlobalLink] = useState({})
+function App() {
+  const [user, setUser] = useState(getUser());
+  const [domainName, setDomainName] = useState('aa-shorty.herokuapp.com/');
+  const [globalLink, setGlobalLink] = useState({});
   const [newGlobalLink, setNewGlobalLink] = useState({
     url: '',
     linkTree: false
-  })
+  });
 
-  const [showShortenedUrl, setShowShortenedUrl] = useState(false)
-  const [signUpModal, setSignUpModal] = useState(false)
-  const [showNewShortyForm, setShowNewShortyForm] = useState(false)
-  const [linkTreeToggled, setLinkTreeToggled] = useState(false)
+  const [showShortenedUrl, setShowShortenedUrl] = useState(false);
+  const [signUpModal, setSignUpModal] = useState(false);
+  const [showNewShortyForm, setShowNewShortyForm] = useState(false);
+  const [linkTreeToggled, setLinkTreeToggled] = useState(false);
 
   const createGlobalLink = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       const response = await fetch('/api/links', {
         method: 'POST',
@@ -29,28 +29,28 @@ function App () {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ ...newGlobalLink })
-      })
-      const data = await response.json()
-      setGlobalLink(data)
-      setShowShortenedUrl(true)
+      });
+      const data = await response.json();
+      setGlobalLink(data);
+      setShowShortenedUrl(true);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const handleChange = (evt) => {
-    setNewGlobalLink({ ...newGlobalLink, [evt.target.name]: evt.target.value })
-  }
+    setNewGlobalLink({ ...newGlobalLink, [evt.target.name]: evt.target.value });
+  };
 
-  const [userLink, setUserLink] = useState('')
+  const [userLink, setUserLink] = useState('');
   const [newUserLink, setNewUserLink] = useState({
     url: '',
     linkTree: false,
     title: ''
-  })
+  });
 
   const createUserLink = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       const response = await fetch(`/api/links/${user._id}`, {
         method: 'POST',
@@ -60,31 +60,31 @@ function App () {
         body: JSON.stringify({
           ...newUserLink
         })
-      })
-      const data = await response.json()
-      setUserLink(data)
-      setShowShortenedUrl(true)
+      });
+      const data = await response.json();
+      setUserLink(data);
+      setShowShortenedUrl(true);
 
-      setShowNewShortyForm(false)
+      setShowNewShortyForm(false);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const handleUserLinkChange = (evt) => {
     setNewUserLink({
       ...newUserLink,
       [evt.target.name]: evt.target.value
-    })
-  }
+    });
+  };
 
-  useEffect(() => {}, [])
+  useEffect(() => {}, []);
 
   return (
     <>
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <HomePage
               user={user}
@@ -108,40 +108,38 @@ function App () {
             />
           }
         />
-        <Route path='/u/:name' element={<LinkTree domainName={domainName} />} />
+        <Route path="/u/:name" element={<LinkTree domainName={domainName} />} />
 
-        {user
-          ? (
-            <Route
-              path='/u/dashboard'
-              element={
-                <UserDashboard
-                  user={user}
-                  domainName={domainName}
-                  setUser={setUser}
-                  showShortenedUrl={showShortenedUrl}
-                  setShowShortenedUrl={setShowShortenedUrl}
-                  userLink={userLink}
-                  setUserLink={setUserLink}
-                  newUserLink={newUserLink}
-                  setNewUserLink={setNewUserLink}
-                  createUserLink={createUserLink}
-                  handleUserLinkChange={handleUserLinkChange}
-                  setNewGlobalLink={setNewGlobalLink}
-                  showNewShortyForm={showNewShortyForm}
-                  setShowNewShortyForm={setShowNewShortyForm}
-                  linkTreeToggled={linkTreeToggled}
-                  setLinkTreeToggled={setLinkTreeToggled}
-                />
+        {user ? (
+          <Route
+            path="/u/dashboard"
+            element={
+              <UserDashboard
+                user={user}
+                domainName={domainName}
+                setUser={setUser}
+                showShortenedUrl={showShortenedUrl}
+                setShowShortenedUrl={setShowShortenedUrl}
+                userLink={userLink}
+                setUserLink={setUserLink}
+                newUserLink={newUserLink}
+                setNewUserLink={setNewUserLink}
+                createUserLink={createUserLink}
+                handleUserLinkChange={handleUserLinkChange}
+                setNewGlobalLink={setNewGlobalLink}
+                showNewShortyForm={showNewShortyForm}
+                setShowNewShortyForm={setShowNewShortyForm}
+                linkTreeToggled={linkTreeToggled}
+                setLinkTreeToggled={setLinkTreeToggled}
+              />
             }
-            />
-            )
-          : (
-            <Route path='/u/dashboard' element={<Navigate to='/' />} />
-            )}
+          />
+        ) : (
+          <Route path="/u/dashboard" element={<Navigate to="/" />} />
+        )}
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
